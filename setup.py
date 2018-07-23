787585827a0a628e14ec7e6f7a206a10dcdc9e4c
ross mckinney <ross.m.mckinney@gmail.com>
2018-07-23 15:54:10 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,63 @@
+DESCRIPTION = (
+    'Package for the tracking, classification, and analysis of courtship ' +
+    'behavior in D. melanogaster'
+    )
+VERSION = '0.1.0'
+DISTNAME = 'courtship'
+AUTHOR = 'Ross McKinney'
+AUTHOR_EMAIL = 'ross.m.mckinney@gmail.com'
+LICENSE = 'MIT'
+
+# These can also be found in requirements.txt
+REQUIRED_PACKAGES = {
+    'alabaster': 'alabaster',
+    'matplotlib': 'matplotlib',
+    'numpy': 'numpy',
+    'pandas': 'pandas',
+    'qt': 'PyQt5',
+    'sklearn': 'scikit-learn',
+    'skimage': 'scikit-image',
+    'scipy': 'scipy',
+    'sphinx': 'sphinx',
+    'sphinxcontrib': 'sphinxcontrib',
+    'motmot.FlyMovieFormat': 'motmot.FlyMovieFormat',
+    'pycircstat': 'pycircstat',
+    'recommonmark': 'recommonmark'
+}
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+def check_dependencies():
+    """Make sure all necessary packages are present."""
+    install = []
+    for key, value in REQUIRED_PACKAGES.iteritems():
+        try:
+            module = __import__(key)
+        except ImportError:
+            install.append(value)
+
+    return install
+
+if __name__ == '__main__':
+    # install_requires = check_dependencies()
+
+    setup(
+        name=DISTNAME,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        maintainer=AUTHOR,
+        maintainer_email=AUTHOR_EMAIL,
+        description=DESCRIPTION,
+        license=LICENSE,
+        version=VERSION,
+        # install_requires=install_requires,
+        packages=[
+                'courtship',
+                'courtship.ml',
+                'courtship.stats',
+                'courtship.app',
+            ]
+    )
