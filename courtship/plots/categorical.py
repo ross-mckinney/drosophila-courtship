d56105438c95a16dba235c529e1cfb90dd02859e
ross mckinney <ross.m.mckinney@gmail.com>
2018-08-17 17:06:50 -0500

    added file for categorical data

---
+++ courtship/plots/categorical.py
@@ -0,0 +1,25 @@
+# courtship.plots.categorical.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import pycircstat.descriptive as pysum
+
+import utils
+
+
+def boxplot(
+    data,
+    at=None,
+    **box_args
+    ):
+    pass
+
+
+def striphist(
+    data,
+    at=None,
+    **kwargs
+    ):
+    pass
