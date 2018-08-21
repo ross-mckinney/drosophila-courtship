94efc1b5f771bdea05c1ea6bb938b0169df1430d
ross mckinney <ross.m.mckinney@gmail.com>
2018-08-21 18:08:42 -0500

    added new tests for new functions

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -10,9 +10,28 @@ if __name__ == '__main__':
         groups={'control': ['tnt-minus'], 'experimental': ['tnt-plus']},
         order=['control', 'experimental']
     )
+    exp.add_behavior_from_csv(
+        behavior_name='courtship_gt',
+        csv_filename='D:/lc-tnt-control/data/hand-scored-courtship.csv')
 
-    exp.get_ang_location_summary('scissoring')
-    exp.get_ang_rvals('scissoring')
-    exp.get_ang_rayleighvals('scissoring')
-    exp.get_ang_watsonwilliams('scissoring')
-    exp.get_behavioral_distances('scissoring')
+    # exp.get_ang_location_summary('scissoring')
+    # exp.get_ang_rvals('scissoring')
+    # exp.get_ang_rayleighvals('scissoring')
+    # exp.get_ang_watsonwilliams('scissoring')
+
+    # exp.get_behavioral_distances('scissoring')
+    # exp.get_behavioral_distances_peak_ratio('scissoring')
+    # exp.get_behavioral_indices('scissoring')
+    # exp.get_behavioral_index_as_fraction_of_courtship(
+    #     behavior_name='scissoring',
+    #     courtship_behavior_name='courtship_gt')
+    # exp.get_behavioral_matrices('courtship_gt')
+    # exp.get_behavioral_latencies('scissoring')
+
+    # exp.get_binned_forward_velocities('courtship_gt')
+    # exp.get_binned_sideways_velocities('courtship_gt')
+    # exp.get_binned_abs_sideways_velocities('courtship_gt')
+
+    exp.save_summary_csv(
+        'test_summary.csv', ['tapping', 'orienting', 'scissoring'],
+        'courtship_gt')
